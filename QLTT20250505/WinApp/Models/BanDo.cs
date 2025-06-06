ef2a8f809daf66bcbe60744b44c82b64bac64be1
Thinh Dang Duc <thinhdangduc4@gmail.com>
2025-06-06 11:34:20 +0700

    lll

---
+++ QLTT20250505/WinApp/Models/BanDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    partial class BanDoPhanBo
+    {
+        public string TenBanDo => Provider.GetTable<BanDoPhanBo>().Find<BanDoPhanBo>(CoSoID).TenBanDo;
+        
+    }
+}
--- QLTT20250505/WinApp/Views/BanDo/BanDoManager.cs
+++ QLTT20250505/WinApp/Views/BanDo/BanDoManager.cs
@@ -29,4 +29,5 @@ namespace WinApp.Views.BanDo
             };
         }
     }
+
     }
